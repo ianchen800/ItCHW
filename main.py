be7ebdd0f7f48e382f44c47d445391817379b04a
ohohmymygodgod <alleneayne@gmail.com>
2019-12-28 20:07:55 +0800

    help

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from args import get_args
 
 
 if __name__ == '__main__':
-    args = get_args()
-    crawler = Crawler()
-    content = crawler.crawl(args.start_date, args.end_date)
+	args = get_args()
+	crawler = Crawler()
+	contents = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
+	with open(args.output, 'w') as f:
+		for date, title, content in contents:
+			title = title.replace('\n', ' ').replace('\r', '')
+			title = title.replace('\"', '\"\"')
+			content = content.replace('\n', ' ').replace('\r', '')
+			content = content.replace('\"', '\"\"')
+			out_str = f'{str(date)}, "{title}", "{content}"\n'
+			f.write(out_str)
